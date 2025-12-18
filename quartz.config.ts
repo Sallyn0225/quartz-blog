f2a4ccf04db94a6cd49f6c9497b22a8f137150fe
ZHOU QIAOFEI <z1921531571@outlook.com>
2025-12-18 12:04:29 +0800

    更改了新的域名 sallyn.top

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "quartz.sallyn.site",
+    baseUrl: "sallyn.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
